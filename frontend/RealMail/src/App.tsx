499827ff443e84f2e258ccca0764c88235521e49
Jeremy Peros <jeremy.peros@gmail.com>
2024-02-29 00:03:06 -0800

    Fleshed out basic homepage

--- frontend/RealMail/src/App.tsx
+++ frontend/RealMail/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Title from './components/title.tsx'
+import Display from "./components/display.tsx";
 
 function App() {
   return (
     <>
       <div>
+          <Title />
+          <Display />
       </div>
     </>
   )
