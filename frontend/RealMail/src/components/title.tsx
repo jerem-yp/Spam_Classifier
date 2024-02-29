4106ab4ac34561c2c9a6fc59d69992fabad661a2
Jeremy Peros <jeremy.peros@gmail.com>
2024-02-29 00:04:51 -0800

    Title. Uses Russo One font

---
+++ frontend/RealMail/src/components/title.tsx
@@ -0,0 +1,15 @@
+import "./title.css"
+
+function Title() {
+    return (
+    <>
+        <div className="center">
+            <div className="content">
+                <p>RealMail&trade;</p>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Title;
